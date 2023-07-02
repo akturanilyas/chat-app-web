725d4d2631ad8b57d46edad6c920acd82a2c19a6
Ilyas AKTURAN <akturanilyas@gmail.com>
2023-07-02 19:36:42 +0300

    Initial commit

--- src/routes/mainRoute.ts
+++ src/routes/mainRoute.ts
@@ -1,5 +1,17 @@
 import { IRoute } from './route.interface';
+import { MAIN_PATH } from '../constants/mainPath.constant';
+import { createElement } from 'react';
+import Chat from '../views/main/chat/Chat';
 
-export const mainRoute: Array<IRoute> = [];
+export const mainRoute: Array<IRoute> = [
+  {
+    path: MAIN_PATH.DASHBOARD,
+    element: createElement(Chat),
+  },
+  {
+    path: '/test',
+    element: createElement(Chat),
+  },
+];
 
 export default mainRoute;
---
+++ src/views/main/chat/Chat.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import BaseView from '../../../components/common/base-view/BaseView';
+import BaseText from '../../../components/common/base-text/BaseText';
+
+const Chat: FC = () => (
+  <BaseView>
+    <BaseText text={'asdas'} />
+  </BaseView>
+);
+
+export default Chat;
